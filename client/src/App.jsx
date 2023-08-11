289c597b3fc6efbb453773055366b31478f76ee6
unknown <madolche2003@gmail.com>
2023-08-12 00:41:57 +0700

    privateroute and searchbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,14 @@ import { useRefreshMutation } from './redux/actions/authAction'
 import Register from './pages/register'
 import Login from './pages/login'
 import Home from './pages/home'
-import Profile from './pages/profile'
 import Post from './pages/post'
 import Header from './components/header/Header'
 import NotFound from './pages/notFound'
+import Message from './pages/message/message'
+import Discover from './pages/discover'
+import Profile from './pages/profile/[id]'
 
-
+import PrivateRouter from './redux/actions/PrivateRouter'
 import Alert from './components/alert/Alert'
 
 function App() {
@@ -37,27 +39,34 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <>
       {token && <Header />}
       <Alert />
 
       <Routes>
-        <Route exact path="/" element={token ? <Home /> : <Login />} />
-        <Route exact path="/register" element={<Register />} />
+        <Route path="/" element={token ? <Home /> : <Login />} />
+        <Route path="/register" element={<Register />} />
 
-        <Route
-          path="/profile/:userId"
-          // element={isAuth ? <Profile /> : <Navigate to="/" />}
-          element={<Profile />}
-        />
-        <Route
-          path="/post/:postId"
-          // element={isAuth ? <Post /> : <Navigate to="/" />}
-          element={<Post />}
-        />
-        <Route path="*" element={<NotFound />} />
+        <Route exact path='/' element={<PrivateRouter />}>
+
+          <Route path='/message' element={<Message />} />
+          <Route path='/discover' element={<Discover />} />
+          <Route
+            path="/profile/:userId"
+            // element={isAuth ? <Profile /> : <Navigate to="/" />}
+            element={<Profile />}
+          />
+          <Route
+            path="/post/:postId"
+            // element={isAuth ? <Post /> : <Navigate to="/" />}
+            element={<Post />}
+          />
+          <Route path="*" element={<NotFound />} />
+
+        </Route>
       </Routes>
-    </div>
+
+    </>
   )
 }
 
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { useLogoutMutation } from '../../redux/actions/authAction'
@@ -11,6 +11,7 @@ import { setSuccess, setError } from '../../redux/reducers/notifyReducer'
 import { toast } from 'react-toastify';
 
 import Avatar from '../Avatar'
+import SearchBar from './SearchBar'
 
 const Header = () => {
     const navigation = [
@@ -53,7 +54,7 @@ const Header = () => {
                 {({ open }) => (
                     <>
                         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                            <div className="relative flex h-16 items-center justify-between">
+                            <div className="relative flex h-16 items-center justify-center grid grid-cols-8">
                                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                     {/* Mobile menu button*/}
                                     <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -66,7 +67,9 @@ const Header = () => {
                                         )}
                                     </Disclosure.Button>
                                 </div>
-                                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
+                                {/* left header */}
+                                <div className="flex flex-1 items-center sm:justify-start sm:items-stretch sm:col-span-3 sm:w-full">
                                     <div className="flex flex-shrink-0 items-center">
                                         <img
                                             className="h-8 w-auto"
@@ -77,22 +80,31 @@ const Header = () => {
                                     <div className="hidden sm:ml-6 sm:block">
                                         <div className="flex space-x-4">
                                             {navigation.map((item) => (
+
                                                 <Link
                                                     key={item.name}
                                                     to={item.path}
                                                     className={classNames(
                                                         isCurrent(item.path) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                        'rounded-md px-3 py-2 text-sm font-medium'
+                                                        'rounded-md px-3 py-2 text-sm font-medium cursor-pointer'
                                                     )}
                                                     aria-current={isCurrent(item.path) ? 'page' : undefined}
                                                 >
                                                     {item.name}
                                                 </Link>
+
                                             ))}
                                         </div>
                                     </div>
                                 </div>
-                                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+                                {/* searchbar */}
+                                <div className="hidden flex-1 sm:col-span-3 sm:w-full sm:flex sm:justify-center sm:items-stretch" >
+                                    <SearchBar />
+                                </div>
+
+                                {/* right header */}
+                                <div className="absolute sm:justify-end inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:col-span-2">
                                     <button
                                         type="button"
                                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -165,7 +177,7 @@ const Header = () => {
                         <Disclosure.Panel className="sm:hidden">
                             <div className="space-y-1 px-2 pb-3 pt-2">
                                 {navigation.map((item) => (
-                                    <Disclosure.Button
+                                    <div
                                         key={item.name}
                                         as="a"
                                         href={item.href}
@@ -175,14 +187,24 @@ const Header = () => {
                                         )}
                                         aria-current={item.current ? 'page' : undefined}
                                     >
-                                        {item.name}
-                                    </Disclosure.Button>
+                                        <Link
+                                            key={item.name}
+                                            to={item.path}
+                                            className={classNames(
+                                                isCurrent(item.path) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                'rounded-md px-3 py-2 text-sm font-medium cursor-pointer'
+                                            )}
+                                            aria-current={isCurrent(item.path) ? 'page' : undefined}
+                                        >
+                                            {item.name}
+                                        </Link>
+                                    </div>
                                 ))}
                             </div>
                         </Disclosure.Panel>
                     </>
                 )}
-            </Disclosure>
+            </Disclosure >
         </>
     )
 }
