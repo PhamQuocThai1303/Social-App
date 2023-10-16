4965541f668f426ee80e31c0ab5c45b435b0f66a
unknown <madolche2003@gmail.com>
2023-10-16 21:36:03 +0700

    update test deploy

--- server.js
+++ server.js
@@ -72,6 +72,13 @@ app.all('*', (req, res) => { //cath-all middleware that go wrong path
 
 app.use(errorHandler)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 mongoose.connection.once('open', () => {
     console.log("connected to MongoDB");
     server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
