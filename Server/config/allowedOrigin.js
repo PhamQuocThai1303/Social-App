399a88ee2b1d225a56af5fd355ead3940ae04f49
unknown <madolche2003@gmail.com>
2023-10-16 23:46:47 +0700

    update CORS

--- Server/config/allowedOrigin.js
+++ Server/config/allowedOrigin.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:5173',
     'https://social-app-api.vercel.app',
-    'https://t-connect-beta.vercel.app/'
+    'https://t-connect-beta.vercel.app'
 ]
 
 
--- client/src/utils/config.jsx
+++ client/src/utils/config.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5173'
+export const BASE_URL = 'https://t-connect-beta.vercel.app/'
 export const ROLES = {
     User: 'user',
     Admin: 'admin'
