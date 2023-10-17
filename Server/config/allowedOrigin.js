87816e05bdbf192dbb52e666f2a0159f8a802eac
unknown <madolche2003@gmail.com>
2023-10-17 21:22:21 +0700

    update

--- Server/config/allowedOrigin.js
+++ Server/config/allowedOrigin.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'https://t-connect-nine.vercel.app',
     'http://localhost:3500',
     'http://localhost:5173',
-    "https://api.example.com/socket.io/?EIO=4&transport=polling"
+    "https://social-app-api.vercel.app/socket.io/?EIO=4&transport=polling"
 ]
 
 
