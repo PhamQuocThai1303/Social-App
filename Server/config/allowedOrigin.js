e104e1a8f93f9a8470bcfdf1b56f7f7feba52315
unknown <madolche2003@gmail.com>
2023-10-17 16:20:20 +0700

    update

--- Server/config/allowedOrigin.js
+++ Server/config/allowedOrigin.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'https://t-connect-beta.vercel.app',
     'http://localhost:3500',
     'http://localhost:5173',
+    'https://social-app-mpgil7nb9-pham-quoc-thais-projects.vercel.app/'
 ]
 
 
